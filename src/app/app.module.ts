4085efabe54ea0fa9fbc9f5bb07b1e72ee0bab34
Kirtan Tank <tankkirtan5@gmail.com>
2022-09-25 22:47:23 +0530

    Login, Register Component Crated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ResultComponent } from './result/result.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ResultComponent
+    ResultComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
