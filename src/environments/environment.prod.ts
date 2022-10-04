85a06a871d35536f080f96a83dbf5ae131086c44
Kirtan Tank <tankkirtan5@gmail.com>
2022-10-04 16:23:30 +0530

    production error solve

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+  apiKey: "AIzaSyD4GymOu2vS_eAtMl1_BcGKyVZGZVvEewU",
+  authDomain: "hndk-solutions.firebaseapp.com",
+  databaseURL: "https://hndk-solutions-default-rtdb.firebaseio.com",
+  projectId: "hndk-solutions",
+  storageBucket: "hndk-solutions.appspot.com",
+  messagingSenderId: "589019280099",
+  appId: "1:589019280099:web:eab83d41947ce71c5b0bd6",
+  measurementId: "G-K4WM8QZ2G9"
+  }
 };
