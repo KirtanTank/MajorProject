1d540465112e82221104eeb9cadc114a2a5b871c
Kirtan Tank <tankkirtan5@gmail.com>
2022-11-04 17:14:39 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {APP_BASE_HREF} from '@angular/common';
+import {LocationStrategy, HashLocationStrategy , APP_BASE_HREF} from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
